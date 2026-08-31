8f28b8feb87367e274d4e6f7a4f0de8ccec89035
mudasir <mudasirkhan842kohat@gmail.com>
2026-08-31 16:39:40 +0500

    Fix: Uncommented create_all to generate database tables

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ from .config import settings
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-engine=create_engine(SQLALCHEMY_DATABASE_URL)
+engine=create_engine(SQLALCHEMY_DATABASE_URL,           pool_pre_ping=True,pool_recycle=1800)
 
 sessionlocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
