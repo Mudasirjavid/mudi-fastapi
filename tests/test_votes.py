7ca436dc5fdc37701aad2687fdc3297a2c8e10be
Mudasir javid <mudasirkhan842kohat@gmail.com>
2026-09-08 17:23:24 +0500

    fixed pytest fixture naming error

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -3,8 +3,8 @@ from app import models
 
 
 @pytest.fixture()
-def test_vote(test_user, session, test_post):
-    new_vote = models.Vote(post_id=test_post[3].id, user_id=test_user["id"])
+def test_vote(test_user, session, test_posts):
+    new_vote = models.Vote(post_id=test_posts[3].id, user_id=test_user["id"])
     session.add(new_vote)
     session.commit()
 
